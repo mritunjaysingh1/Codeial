4ade1330cf8d287ef25a9a7e5e5f4f2b79981800
Mritunjay <mritunjaysingh045@gmail.com>
2022-12-16 11:29:28 +0530

    added comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,17 +12,25 @@ module.exports.home = function(req, res){
     // });
 
 
-    Post.find({})
-    .populate('user')
-    .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
-        });
-    })
-
+      // populate the user of each post
+      Post.find({})
+      .populate('user')
+      .populate({
+          path: 'comments',
+          populate: {
+              path: 'user'
+          }
+      })
+      .exec(function(err, posts){
+          return res.render('home', {
+              title: "Codeial | Home",
+              posts:  posts
+          });
+      })
+  
+  }
 
     
-}
+
 
 // module.exports.actionName = function(req, res){}
