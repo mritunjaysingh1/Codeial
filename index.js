509aa4dc42e792c2cdd0567bc2853243d94eca43
Mritunjay <mritunjaysingh045@gmail.com>
2022-12-27 21:38:46 +0530

    Social Authentication

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-startegy');
+const passportGoogle = require('./config/passport-google-auth-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware =require('node-sass-middleware');
 const flash = require('connect-flash');
